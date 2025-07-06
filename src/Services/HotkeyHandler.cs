ab95118279c76ba4051e9b7c2268d37be1472f47
AnatoliiUtochkin <clashertolia@gmail.com>
2025-07-06 20:01:12 +0200

    Added popup window for translated text

--- src/Services/HotkeyHandler.cs
+++ src/Services/HotkeyHandler.cs
@@ -1,4 +1,5 @@
 ﻿using QuickLingo.Helpers;
+using QuickLingo.Views.Popups;
 using System.Windows;
 
 namespace QuickLingo.Services
@@ -11,16 +12,33 @@ namespace QuickLingo.Services
         {
             string selectedText = await _clipboardHelper.TryCopySelectedTextAsync();
 
-            if (string.IsNullOrWhiteSpace(selectedText))
-            {
-                System.Windows.MessageBox.Show("Не вдалося отримати виділений текст. Скопіюйте його вручну або перевірте фокус вікна.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-
             TranslatorService translatorService = new();
             string translatedText = await translatorService.TranslateAsync(selectedText);
 
-            System.Windows.MessageBox.Show($"Переклад вибраного тексту: {translatedText}");
+            var translationWindow = new TranslationPopup(translatedText);
+
+            var mousePosition = Control.MousePosition;
+
+            var screen = Screen.FromPoint(mousePosition);
+
+            double left = mousePosition.X + 15;
+            double top = mousePosition.Y + 15;
+
+            if (left + translationWindow.Width > screen.WorkingArea.Right)
+            {
+                left = screen.WorkingArea.Right - translationWindow.Width;
+            }
+
+            if (top + translationWindow.Height > screen.WorkingArea.Bottom)
+            {
+                top = screen.WorkingArea.Bottom - translationWindow.Height;
+            }
+
+            translationWindow.Left = left;
+            translationWindow.Top = top;
+
+            translationWindow.Show();
+            translationWindow.Activate();
 
             _clipboardHelper.RestorePreviousClipboard();
         }
---
+++ src/Views/Popups/TranslationPopup.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuickLingo.Views.Popups
+{
+    /// <summary>
+    /// Interaction logic for TranslationPopup.xaml
+    /// </summary>
+    public partial class TranslationPopup : Window
+    {
+        public TranslationPopup(string translationResult)
+        {
+            InitializeComponent();
+            ResultTextBox.Text = translationResult;
+
+            var shadow = new DropShadowEffect
+            {
+                Color = Colors.Black,
+                BlurRadius = 20,
+                ShadowDepth = 0,
+                Opacity = 0.6
+            };
+            this.Resources["DropShadowEffect"] = shadow;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+    }
+}
